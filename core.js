995b25ddd28b9fbb62f7219921c6b8f96191f6b4
3greenfish <curtis.boone@gmail.com>
2025-12-14 21:45:28 -0800

    Update core.js

--- core.js
+++ core.js
@@ -482,6 +482,10 @@ function loadAllContentCosts() {
 	msg("load all content costs called");
 	for (let i = 0; i < swamp.buildings.length; i++) {
 		msg("loading " + i);
+		if (document.getElementById("buy-" + i + "-Costs") == null) {
+			msg("could not find element buy-" + i + "-Costs");
+			continue;
+		}
 		updateContentCosts(i);
 	}
 }
