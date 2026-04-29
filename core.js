305164996938b7efab7db0a48efadc1b6c5a2402
3greenfish <curtis.boone@gmail.com>
2026-04-28 20:59:34 -0700

    Refactor researchBase into TechBase class

--- core.js
+++ core.js
@@ -1054,7 +1054,64 @@ const resourcesBase = {
 
 
 let research = {};
-const researchBase = {
+
+class TechBase {
+	name = "research";
+	stack;
+	constructor() {
+		this.stack = [
+			{ name: "calendar",
+			  label: "Calendar",
+			  desc: "Discover the world's cyclical cycle.",
+			  flavor: null,
+			  costs: [
+				  { name: "corruption", amount: 10 }
+			  ],
+			  purchased: false,
+			  actions: [
+				  { subLabel: "Research",
+					type: "main",
+					press: function(code, isMain = false) {
+						devMsg("purchasing research");
+						let cal = research.stack[code];
+						let getCosts = cal.costs;
+	
+						if (resources.checkCostsByArray(getCosts, 0).result == "pass") {
+							resources.payCostsByArray(getCosts, 0);
+							calendar.activateCal();
+							cal.purchased = true;
+							buildGrid(research, research.stack, true);
+						}
+						else if (isMain == true) {
+							let target = "research" + code;
+							expandButton2(target);
+						}
+					}
+				  }
+			  ],
+			  effects: [],
+			  unlocks: []
+			},
+			{ name: "stone tools",
+			  label: "Stone tools",
+			  desc: "Improve hunting and gathering with tools made of stone.",
+			  flavor: "sometimes you just have to hit something with a rock.",
+			  costs: [
+				  { name: "corruption", amount: 15 }
+			  ],
+			  purchased: false,
+			  lockedBy: [
+				  { type: "tech", name: "calendar" }
+			  ],
+			  actions: [],
+			  effects: [],
+			  unlocks: []
+			}
+		]
+	}
+}
+
+/* const researchBase = {
 	name: "research",
 	stack: [
 		{ name: "calendar",
@@ -1107,7 +1164,7 @@ const researchBase = {
 
 	
 	]
-};
+}; */
 	
 
 
@@ -1132,7 +1189,8 @@ function loadGame() {	//runs at end of HTML load
 	Object.assign(swamp, swampBase);
 	Object.assign(resources, resourcesBase);
 	Object.assign(game, gameBase);
-	Object.assign(research, researchBase);
+	research = new TechBase();
+//	Object.assign(research, researchBase);
 //	research = structuredClone(researchBase);
 	game.buildNav();
 	timing.activateBelt();
